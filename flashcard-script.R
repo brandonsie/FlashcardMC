70dfda4f52a0c2c49093e896a3ca050f470892dc
brandonsie <brandonsie@gmail.com>
2018-11-02 15:07:00 -0400

    added default option to immediately re-quiz for any wrong answers

--- flashcard-script.R
+++ flashcard-script.R
@@ -10,21 +10,31 @@ setupFlashcards <- function(input){
 	#nattempted, correctstreak, timestamp
 	
 	input$NAttempts <- input$CorrectStreak <- 0
+	input$NRight <- input$NWrong <- 0
 	input$TimeStamp <- ""
 	return(input)
 }
 
-quizFlashcards <- function(input, reps = 0, choices = 4, invert = FALSE){
+quizFlashcards <- function(input, reps = 0, focusrows = 0,
+													 choices = 4, invert = FALSE, requiz = TRUE){
 	
 	#if invert, flip prompt and answer
 	if(invert){input <- flipQA(input)}
 	
 	#set random order of prompt
-	lineup <- (1:nrow(input))[order(rnorm(1:nrow(input)))]
+	if(focusrows == 0){
+		lineup <- (1:nrow(input))[order(rnorm(1:nrow(input)))]
+	} else if(length(focusrows) == 1){
+		lineup <- focusrows
+	} else {
+		lineup <- focusrows[order(rnorm(focusrows))]
+	}
+	
 	
 	#loop number of repetitions
 	if(reps == 0 | (reps > nrow(input))){reps <- nrow(input)}
-	nright <- 0; nwrong <- 0
+	nright <- 0; nwrong <- 0; wrongreps <- vector(mode = "numeric")
+	
 	
 	for(i in 1:reps){
 		cat("\n\nQuestion",i,"of",reps)
@@ -35,7 +45,7 @@ quizFlashcards <- function(input, reps = 0, choices = 4, invert = FALSE){
 		wronganswer <- sample(input$Answer[input$Answer != rightanswer],(choices-1))
 
 		#randomize order, keep track of correct position, and prompt user
-		answers <- sample(c(rightanswer,wronganswer)) %>% data.table %>% setnames("")
+		answers <- sample(c(rightanswer,wronganswer)) %>% data.frame %>% setnames("")
 		rightpos <- (1:nrow(answers))[answers==rightanswer]
 		
 		
@@ -50,11 +60,14 @@ quizFlashcards <- function(input, reps = 0, choices = 4, invert = FALSE){
 		} else if(userchoice == rightpos){
 			print("Correct!")
 			input$CorrectStreak[index] %<>% +1
+			input$NRight[index] %<>% +1
 			nright %<>% +1
 		} else{
 			print(paste("Incorrect. The correct choice was",rightpos,":",rightanswer))
 			input$CorrectStreak[index] <- 0
+			input$NWrong[index] %<>% +1
 			nwrong %<>% +1
+			wrongreps %<>% c(index)
 		}
 		
 		input$NAttempts[index] %<>% +1
@@ -63,6 +76,14 @@ quizFlashcards <- function(input, reps = 0, choices = 4, invert = FALSE){
 	}
 	
 	print(paste("Yout got",nright,"correct and",nwrong,"incorrect."))
+	
+	
+	#(!) if requiz, re-test wrong answers
+	if(requiz & length(wrongreps) > 0){
+		input %<>% quizFlashcards(reps = length(wrongreps), focusrows = wrongreps)
+	}
+	
+	
 	
 	#if invert, flip back
 	if(invert){input <- flipQA(input)}
@@ -73,7 +94,7 @@ quizFlashcards <- function(input, reps = 0, choices = 4, invert = FALSE){
 flipQA <- function(input, cols = c(1:2)){
 	output <- data.frame(input)
 	output[,cols] <- output[,rev(cols)]
-	return(data.table(output))
+	return(output)
 }
 
 
@@ -81,7 +102,7 @@ flipQA <- function(input, cols = c(1:2)){
 if(FALSE){
 	#Example run
 	libcallFlashcards()
-	v1 <- fread("vocab1.csv")
+	v1 <- fread("vocab1.csv",data.table=FALSE)
 	v1 %<>% setupFlashcards()
-	v1 %<>% quizFlashcards(10)
+	v1 %<>% quizFlashcards(3)
 }
